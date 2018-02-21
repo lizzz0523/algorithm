fe4402bd151da67f0bf0599490ebcd602ff7db64
Liz Chow <lizzzled@gmail.com>
2018-02-21 23:31:19 +0800

    add std_sort

---
+++ rust/std_sort/src/main.rs
@@ -0,0 +1,23 @@
+extern crate rand;
+
+use std::time::SystemTime;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let now = SystemTime::now();
+    let len = 1_000_000_000;
+
+    let mut vec = Vec::with_capacity(len);
+    let mut rng = thread_rng();
+
+    for _ in 0..len {
+        // 这里不敢放肆用了u8，怕内存撑不住
+        vec.push(rng.gen::<u8>());
+    }
+
+    vec.sort();
+
+    // println!("{:?}", vec);
+    println!("{:?}", now.elapsed());
+    // Ok(Duration { secs: 136, nanos: 319578000 })
+}
